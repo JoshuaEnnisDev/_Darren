d5554c0fa98bccc5abdf3658944bb02ba62bfd77
JoshuaEnnisDev <joshuaennisdev@gmail.com>
2023-08-14 19:04:52 -0500

    update flake 8

--- pygame/shooting_game/shooting_game.py
+++ pygame/shooting_game/shooting_game.py
@@ -1,21 +1,54 @@
 import pygame
 import sys
+from random import randint
 
-# General setup
+
+# classes
+class Crosshair(pygame.sprite.Sprite):
+    def __init__(self, image_path):
+        super().__init__()
+        self.image = pygame.image.load(image_path).convert_alpha()
+        self.rect = self.image.get_rect()
+        self.gun_sound = pygame.mixer.Sound('sounds/gunshot.wav')
+
+    def shoot(self):
+        self.gun_sound.play()
+        pygame.sprite.spritecollide(crosshair, target_group, True)
+
+    def update(self):
+        self.rect.center = pygame.mouse.get_pos()
+
+
+class Target(pygame.sprite.Sprite):
+    def __init__(self, image_path, pos_x, pos_y):
+        super().__init__()
+        self.image = pygame.image.load(image_path).convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect.center = (pos_x, pos_y)
+
+
+# general setup
 pygame.init()
 clock = pygame.time.Clock()
 
-# Game_screen
+# game_screen
 WIDTH = 1024
 HEIGHT = 900
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Enter title here")
+pygame.display.set_caption("Target Practice")
 
-# test_surf = pygame.Surface((50, 50))
-# test_surf.fill("black")
-# test_rect = test_surf.get_rect(center=(100, 100))
-test_image = pygame.image.load('graphics/crosshair.png')
-test_rect = test_image.get_rect(center=(100, 100))
+pygame.mouse.set_visible(False)
+
+# crosshair
+crosshair = Crosshair("graphics/crosshair.png")
+crosshair_group = pygame.sprite.Group()
+crosshair_group.add(crosshair)
+
+# target
+target_group = pygame.sprite.Group()
+for _ in range(20):
+    target_group.add(Target('graphics/target.png', randint(20, WIDTH), randint(0, HEIGHT)))
+
 
 # event loop
 while True:
@@ -23,8 +56,11 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            crosshair.shoot()
     screen.fill((150, 200, 100))
-    screen.blit(test_image, test_rect)
-    test_rect.x += 1
+    target_group.draw(screen)
+    crosshair_group.draw(screen)
+    crosshair_group.update()
     pygame.display.update()
     clock.tick(60)
