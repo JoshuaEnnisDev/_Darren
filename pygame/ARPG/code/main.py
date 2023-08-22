e6f4d381d724c7f5fab596be6103aafe6caea0c5
JoshuaEnnisDev <joshuaennisdev@gmail.com>
2023-08-21 19:21:57 -0500

    commit

--- pygame/ARPG/code/main.py
+++ pygame/ARPG/code/main.py
@@ -1,6 +1,6 @@
 import pygame, sys
 from settings import *
-# from debug import debug
+from level import Level
 
 
 class Game:
@@ -11,6 +11,8 @@ class Game:
         pygame.display.set_caption('Zelda')
         self.clock = pygame.time.Clock()
 
+        self.level = Level()
+
     def run(self):
         while True:
             for event in pygame.event.get():
@@ -18,6 +20,7 @@ class Game:
                     pygame.quit()
                     sys.exit()
             self.screen.fill('black')
+            self.level.run()
             pygame.display.update()
             self.clock.tick(FPS)
 
