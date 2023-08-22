d412f65612a23aa3c55b56c8b54ee7517adba77a
JoshuaEnnisDev <joshuaennisdev@gmail.com>
2023-08-21 21:03:32 -0500

    Up to player movement

--- pygame/ARPG/code/player.py
+++ pygame/ARPG/code/player.py
@@ -6,3 +6,6 @@ class Player(pygame.sprite.Sprite):
         super().__init__(groups)
         self.image = pygame.image.load('../graphics/test/player.png').convert_alpha()
         self.rect = self.image.get_rect(topleft=pos)
+
+
+# Start back in creating
