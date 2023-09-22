a3f6afee1c3606e0ee838779214e8d8478466447
JoshuaEnnisDev <joshuaennisdev@gmail.com>
2023-09-21 20:17:53 -0500

    commit

--- pygame/ARPG/code/level.py
+++ pygame/ARPG/code/level.py
@@ -28,7 +28,7 @@ class Level:
                     self.player = Player((x, y), [self.visible_sprites], self.obstacle_sprites)
 
     def run(self):
-        self.visible_sprites.custom_draw(self.display_surface)
+        self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
         debug(self.player.direction)
 
@@ -37,7 +37,14 @@ class YSortCameraGroup(pygame.sprite.Group):
     def __init__(self):
         super().__init__()
         self.display_surface = pygame.display.get_surface()
+        self.half_width = self.display_surface.get_width() // 2
+        self.half_height = self.display_surface.get_height() // 2
+        self.offset = pygame.math.Vector2()
 
     def custom_draw(self, player):
+        self.offset.x = player.rect.centerx - self.half_width
+        self.offset.y = player.rect.centery - self.half_height
+        
         for sprite in self.sprites():
-            self.display_surface.blit(sprite.image, sprite.rect)
+            offset_pos = sprite.rect.topleft - self.offset
+            self.display_surface.blit(sprite.image, offset_pos)
